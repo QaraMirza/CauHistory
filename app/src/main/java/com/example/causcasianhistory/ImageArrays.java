2dbb3bee013f2fbfd24cd3b1b21b38cdba674277
QaraMirza <mirzabek05dag@mail.ru>
2022-04-18 00:12:35 +0300

    Begin

---
+++ app/src/main/java/com/example/causcasianhistory/ImageArrays.java
@@ -0,0 +1,54 @@
+package com.example.causcasianhistory;
+
+public class ImageArrays {
+    static int[] iventImages = {
+            0,
+            0,
+            0,
+            0,
+            0
+    };
+    static int[] personImages = {
+            R.drawable.sultanmahmud,
+            R.drawable.hadjidavud,
+            R.drawable.shaikhmansour,
+            R.drawable.sheretlukokyzbech,
+            0,
+            0,
+            0,
+            R.drawable.imamshamil,
+            R.drawable.alibekhadji,
+            R.drawable.gocinsky2
+    };
+
+
+    static int[] ethnoImages = {
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0
+    };
+    static int[] stateImages = {
+            R.drawable.imamate,
+            R.drawable.emirate,
+            0,
+            R.drawable.lakflag,
+            R.drawable.awarenflag,
+            0
+    };
+    static int[] cityImages = {
+            R.drawable.makhachkala,
+            R.drawable.darband,
+            R.drawable.temirhanshura,
+            R.drawable.hasavurt,
+            0,
+            0,
+            R.drawable.kizliar,
+            R.drawable.kizilurt,
+            0
+    };
+}
